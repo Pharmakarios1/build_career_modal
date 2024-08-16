2da890da41bf31cf326f90c3621859be1d67b408
pharmakarios1 <akhigbeblessed@gmail.coom>
2024-08-16 11:42:23 +0100

    simple animated  modal

---
+++ script.js
@@ -0,0 +1,19 @@
+const modal = document.querySelector(".modal");
+const modalButton = document.querySelector(".modal_button");
+const Modalclose = document.querySelector(".fa-xmark");
+const modalOverlay = document.querySelector(".modal_overlay");
+
+const handleClose = () => {
+  modal.style.display = "none";
+  modalButton.style.display = "block";
+  modalOverlay.style.display = "none";
+};
+
+const handleOpen = () => {
+  modal.style.display = "block";
+  modalButton.style.display = "none";
+  modalOverlay.style.display = "block";
+};
+
+Modalclose.addEventListener("click", handleClose);
+modalButton.addEventListener("click", handleOpen);
